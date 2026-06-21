56cafcede22d9d651d8b27c29d3c73581c58899f
Kirill Nesmeyanov <nesk@xakep.ru>
2026-06-21 05:18:30 +0300

    Fix types linter issues

--- libs/types/src/Type/NamedTypeNode.php
+++ libs/types/src/Type/NamedTypeNode.php
@@ -4,23 +4,15 @@ declare(strict_types=1);
 
 namespace TypeLang\Node\Type;
 
-use TypeLang\Node\Identifier;
 use TypeLang\Node\Name;
 use TypeLang\Node\Type\Shape\FieldsListNode;
 use TypeLang\Node\Type\Template\TemplateArgumentsListNode;
 
 class NamedTypeNode extends TypeNode
 {
-    public Name $name;
-
-    /**
-     * @param Name|Identifier|non-empty-string $name
-     */
     public function __construct(
-        Name|Identifier|string $name,
+        public Name $name,
         public ?TemplateArgumentsListNode $arguments = null,
         public ?FieldsListNode $fields = null,
-    ) {
-        $this->name = $name instanceof Name ? $name : new Name($name);
-    }
+    ) {}
 }
