5473008c5a5d7e5e4e36ae8de4cce0bcc3407760
Kirill Nesmeyanov <nesk@xakep.ru>
2026-07-04 22:18:03 +0300

    Add throws tag

---
+++ libs/phpdoc/src/DocBlock/Tag/ThrowsTag/ThrowsTag.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TypeLang\PhpDoc\DocBlock\Tag\ThrowsTag;
+
+use TypeLang\PhpDoc\DocBlock\Tag\TypedTag;
+
+/**
+ * The "@throws" tag indicates the type of "\Throwable" that an element may
+ * throw.
+ */
+final class ThrowsTag extends TypedTag {}
---
+++ libs/phpdoc/src/DocBlock/Tag/ThrowsTag/ThrowsTagDefinition.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TypeLang\PhpDoc\DocBlock\Tag\ThrowsTag;
+
+use TypeLang\PhpDoc\DocBlock\Description\DescriptionInterface;
+use TypeLang\PhpDoc\DocBlock\Grammar\DescriptionGrammarRule;
+use TypeLang\PhpDoc\DocBlock\Grammar\TypeGrammarRule;
+use TypeLang\PhpDoc\DocBlock\Tag\TagDefinition;
+use TypeLang\PhpDoc\DocBlock\Type\TypeStatement;
+use TypeLang\PhpDoc\Parser\Grammar\MatchedResult;
+use TypeLang\PhpDoc\Parser\Grammar\Rule\MatchRule;
+use TypeLang\PhpDoc\Parser\Grammar\Rule\Optional;
+use TypeLang\PhpDoc\Parser\Grammar\Rule\SequenceOf;
+
+/**
+ * The "`@throws`" tag indicates that a function or method is able to throw
+ * a specific type of "\Throwable" (an exception or an error).
+ *
+ * The "`@throws`" tag MAY be followed by a description explaining when and why
+ * the "\Throwable" is thrown.
+ *
+ * ```
+ * "@throws" <Type> [ <Description> ]
+ * ```
+ */
+final class ThrowsTagDefinition extends TagDefinition
+{
+    public const string NAME = 'throws';
+
+    public function __construct()
+    {
+        parent::__construct(
+            name: self::NAME,
+            rule: new SequenceOf(
+                new MatchRule(TypeGrammarRule::NAME, 'type'),
+                new Optional(
+                    new MatchRule(DescriptionGrammarRule::NAME, 'description'),
+                ),
+            ),
+            isInline: false,
+        );
+    }
+
+    public function create(string $name, MatchedResult $result): ThrowsTag
+    {
+        /** @var TypeStatement $type */
+        $type = $result->get('type');
+
+        /** @var DescriptionInterface|null $description */
+        $description = $result->find('description');
+
+        return new ThrowsTag(self::NAME, $type, $description);
+    }
+}
