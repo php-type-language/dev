0f00ee72cb00fb9634ea96b5216467314715d576
Kirill Nesmeyanov <nesk@xakep.ru>
2026-07-05 02:35:40 +0300

    Improve URI and add URL + Version combinators

--- libs/phpdoc/src/DocBlock/Combinator/UriCombinator.php
+++ libs/phpdoc/src/DocBlock/Combinator/UriCombinator.php
@@ -9,6 +9,11 @@ use TypeLang\PhpDoc\Parser\Grammar\CombinatorInterface;
 use TypeLang\PhpDoc\Parser\Grammar\Cursor;
 use TypeLang\PhpDoc\Parser\Grammar\Exception\NoMatchException;
 
+/**
+ * Reads a URI, that is a well-formed word as defined by RFC 3986.
+ *
+ * @implements CombinatorInterface<UriReference>
+ */
 final readonly class UriCombinator implements CombinatorInterface
 {
     public const string NAME = 'URI';
@@ -17,10 +22,19 @@ final readonly class UriCombinator implements CombinatorInterface
     {
         $uri = $cursor->readWord();
 
-        if ($uri === '') {
+        if ($uri === '' || !self::isUri($uri)) {
             throw new NoMatchException('Expected a URI');
         }
 
         return new UriReference($uri);
+    }
+
+    private static function isUri(string $word): bool
+    {
+        if (\PHP_VERSION_ID >= 80500) {
+            return \Uri\Rfc3986\Uri::parse($word) !== null;
+        }
+
+        return \parse_url($word) !== false;
     }
 }
