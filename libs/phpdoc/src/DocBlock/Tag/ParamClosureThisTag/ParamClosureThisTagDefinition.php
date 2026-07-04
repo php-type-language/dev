67eab10d3d38a7b95acf81731ca5b406d829082c
Kirill Nesmeyanov <nesk@xakep.ru>
2026-07-04 22:17:31 +0300

    Add param-closure-this tag

---
+++ libs/phpdoc/src/DocBlock/Tag/ParamClosureThisTag/ParamClosureThisTagDefinition.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TypeLang\PhpDoc\DocBlock\Tag\ParamClosureThisTag;
+
+use TypeLang\PhpDoc\DocBlock\Description\DescriptionInterface;
+use TypeLang\PhpDoc\DocBlock\Grammar\DescriptionGrammarRule;
+use TypeLang\PhpDoc\DocBlock\Grammar\TypeGrammarRule;
+use TypeLang\PhpDoc\DocBlock\Grammar\VariableGrammarRule;
+use TypeLang\PhpDoc\DocBlock\Tag\TagDefinition;
+use TypeLang\PhpDoc\DocBlock\Type\TypeStatement;
+use TypeLang\PhpDoc\Parser\Grammar\MatchedResult;
+use TypeLang\PhpDoc\Parser\Grammar\Rule\MatchRule;
+use TypeLang\PhpDoc\Parser\Grammar\Rule\Optional;
+use TypeLang\PhpDoc\Parser\Grammar\Rule\SequenceOf;
+
+/**
+ * The "`@param-closure-this`" tag documents the bound "$this" type of closure
+ * passed as an argument.
+ *
+ * ```
+ * "@param-closure-this" <Type> <Variable> [ <Description> ]
+ * ```
+ */
+final class ParamClosureThisTagDefinition extends TagDefinition
+{
+    public const string NAME = 'param-closure-this';
+
+    public function __construct()
+    {
+        parent::__construct(
+            name: self::NAME,
+            rule: new SequenceOf(
+                new MatchRule(TypeGrammarRule::NAME, 'type'),
+                new MatchRule(VariableGrammarRule::NAME, 'variable'),
+                new Optional(
+                    new MatchRule(DescriptionGrammarRule::NAME, 'description'),
+                ),
+            ),
+            isInline: false,
+        );
+    }
+
+    public function create(string $name, MatchedResult $result): ParamClosureThisTag
+    {
+        /** @var TypeStatement $type */
+        $type = $result->get('type');
+
+        /** @var non-empty-string $variable */
+        $variable = $result->get('variable');
+
+        /** @var DescriptionInterface|null $description */
+        $description = $result->find('description');
+
+        return new ParamClosureThisTag(self::NAME, $type, $variable, $description);
+    }
+}
