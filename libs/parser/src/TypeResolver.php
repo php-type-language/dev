1f02eab51d85cca9acbec0c385a27372428223fe
Kirill Nesmeyanov <nesk@xakep.ru>
2026-07-09 00:45:49 +0300

    Improve type resolver and fix FQN name resolver

--- libs/parser/src/TypeResolver.php
+++ libs/parser/src/TypeResolver.php
@@ -5,13 +5,50 @@ declare(strict_types=1);
 namespace TypeLang\Parser;
 
 use TypeLang\Parser\Traverser\TypeMapVisitor;
+use TypeLang\Parser\TypeResolver\PhpUseStatementsTransformer;
 use TypeLang\Type\TypeNode;
 
-final class TypeResolver implements TypeResolverInterface
+final readonly class TypeResolver
 {
-    public function resolve(TypeNode $type, callable $transform): TypeNode
+    public function __construct(
+        /**
+         * @var array<array-key, non-empty-string>
+         */
+        private array $imports = [],
+    ) {}
+
+    /**
+     * @api
+     * @param non-empty-string $name
+     */
+    public function withTypeImport(string $name): self
     {
-        Traverser::through(new TypeMapVisitor($transform(...)), [$type]);
+        return new self([...$this->imports, $name]);
+    }
+
+    /**
+     * @api
+     * @param non-empty-string $name
+     * @param non-empty-string $alias
+     */
+    public function withTypeImportAs(string $name, string $alias): self
+    {
+        return new self([...$this->imports, $alias => $name]);
+    }
+
+    private function createTraverser(): TraverserInterface
+    {
+        $transformer = new PhpUseStatementsTransformer($this->imports);
+
+        return new Traverser([
+            new TypeMapVisitor($transformer(...)),
+        ]);
+    }
+
+    public function resolve(TypeNode $type): TypeNode
+    {
+        $traverser = $this->createTraverser();
+        $traverser->traverse([$type]);
 
         return $type;
     }
