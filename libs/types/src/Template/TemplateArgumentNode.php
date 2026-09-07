6d50ea8df2c3960c74af6ad5e98faf535cae0bac
SerafimArts <nesk@xakep.ru>
2026-09-07 22:33:35 +0300

    Add AST template argument comments

--- libs/types/src/Template/TemplateArgumentNode.php
+++ libs/types/src/Template/TemplateArgumentNode.php
@@ -8,20 +8,37 @@ use TypeLang\Type\Identifier;
 use TypeLang\Type\Node;
 use TypeLang\Type\TypeNode;
 
+/**
+ * A single template argument, along with the hint it may carry.
+ *
+ * ```
+ *  Some\Any<int, covariant string>
+ *  //       ^^^                    simple argument
+ *  //            ^^^^^^^^^         a hinted argument
+ * ```
+ */
 final class TemplateArgumentNode extends Node
 {
-    public ?Identifier $hint;
-
     /**
      * @param int<0, max> $offset
      */
     public function __construct(
         public TypeNode $value,
-        ?Identifier $hint = null,
+        /**
+         * Word standing in front of the argument, or {@see null} in case of it
+         * carries none.
+         *
+         * What the word means is left to whoever reads the tree. A `covariant`
+         * or an `out` is an ordinary identifier as far as the grammar goes.
+         *
+         * ```
+         *  Some\Any<covariant string>
+         *  //       ^^^^^^^^^ the hint
+         * ```
+         */
+        public ?Identifier $hint = null,
         int $offset = 0,
     ) {
-        $this->hint = $hint;
-
         parent::__construct($offset);
     }
 }
