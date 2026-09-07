c2513ea977f78ecb30b54f242e115ee034dc27f2
SerafimArts <nesk@xakep.ru>
2026-09-07 21:26:57 +0300

    Add support of non-empty node list

--- libs/types/src/NodeList.php
+++ libs/types/src/NodeList.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 namespace TypeLang\Type;
 
 /**
+ * An ordered list of nodes of one kind, like the arguments of a generic
+ * or the fields of a shape.
+ *
  * @template TNode of Node = Node
+ * @template TNonEmpty of bool = false
  *
  * @template-implements \IteratorAggregate<array-key, TNode>
  * @template-implements \ArrayAccess<int<0, max>, TNode>
@@ -16,7 +20,9 @@ abstract class NodeList extends Node implements
     \Countable
 {
     /**
-     * @var list<TNode>
+     * The nodes of a list, in the order they are written in.
+     *
+     * @var (TNonEmpty is true ? non-empty-list<TNode> : list<TNode>)
      */
     public array $items = [];
 
@@ -27,14 +33,38 @@ abstract class NodeList extends Node implements
     public function __construct(iterable $items = [], int $offset = 0)
     {
         $this->items = match (true) {
-            // A list is already shaped the way it is stored, so it is taken
-            // as it is rather than copied
-            \is_array($items) && \array_is_list($items) => $items,
-            \is_array($items) => \array_values($items),
-            default => \iterator_to_array($items, false),
+            $items instanceof \Traversable => \iterator_to_array($items, false),
+            \array_is_list($items) => $items,
+            default => \array_values($items),
         };
 
         parent::__construct($offset);
+    }
+
+    /**
+     * Gets the first node of a list or {@see null} in case of the list is empty.
+     *
+     * @return (TNonEmpty is true ? TNode : TNode|null)
+     */
+    public function first(): ?Node
+    {
+        return $this->items[0] ?? null;
+    }
+
+    /**
+     * Gets the last node of a list or {@see null} in case of the list is empty.
+     *
+     * @return (TNonEmpty is true ? TNode : TNode|null)
+     */
+    public function last(): ?Node
+    {
+        $key = \array_key_last($this->items);
+
+        if ($key === null) {
+            return null;
+        }
+
+        return $this->items[$key];
     }
 
     /**
@@ -61,7 +91,9 @@ abstract class NodeList extends Node implements
     public function offsetExists(mixed $offset): bool
     {
         if (!\is_int($offset) || $offset < 0) {
-            throw new \InvalidArgumentException('A NodeList index must be an int<0, max>');
+            throw new \InvalidArgumentException(
+                \sprintf('A %s::offsetExists($offset) must be an int<0, max>', static::class),
+            );
         }
 
         return isset($this->items[$offset]);
@@ -70,7 +102,9 @@ abstract class NodeList extends Node implements
     public function offsetGet(mixed $offset): ?Node
     {
         if (!\is_int($offset) || $offset < 0) {
-            throw new \InvalidArgumentException('A NodeList index must be an int<0, max>');
+            throw new \InvalidArgumentException(
+                \sprintf('A %s::offsetGet($offset) must be an int<0, max>', static::class),
+            );
         }
 
         return $this->items[$offset] ?? null;
@@ -79,7 +113,9 @@ abstract class NodeList extends Node implements
     public function offsetSet(mixed $offset, mixed $value): void
     {
         if (!$value instanceof Node) {
-            throw new \InvalidArgumentException('A NodeList value must be instance of Node');
+            throw new \InvalidArgumentException(
+                \sprintf('A %s::offsetSet(..., $value) must be instance of %s', static::class, Node::class),
+            );
         }
 
         if ($offset === null) {
@@ -89,7 +125,9 @@ abstract class NodeList extends Node implements
         }
 
         if (!\is_int($offset) || $offset < 0) {
-            throw new \InvalidArgumentException('A NodeList index must be an int<0, max>|null');
+            throw new \InvalidArgumentException(
+                \sprintf('A %s::offsetSet($offset, ...) must be an int<0, max>|null', static::class),
+            );
         }
 
         // @phpstan-ignore-next-line
@@ -103,7 +141,9 @@ abstract class NodeList extends Node implements
     public function offsetUnset(mixed $offset): void
     {
         if (!\is_int($offset) || $offset < 0) {
-            throw new \InvalidArgumentException('A NodeList index must be an int<0, max>|null');
+            throw new \InvalidArgumentException(
+                \sprintf('A %s::offsetUnset($offset) must be an int<0, max>', static::class),
+            );
         }
 
         // @phpstan-ignore-next-line
@@ -120,36 +160,10 @@ abstract class NodeList extends Node implements
     }
 
     /**
-     * @return int<0, max>
+     * @return (TNonEmpty is true ? int<1, max> : int<0, max>)
      */
     public function count(): int
     {
         return \count($this->items);
-    }
-
-    /**
-     * Gets the first node of a list or {@see null} in case of the list is empty.
-     *
-     * @return TNode|null
-     */
-    public function first(): ?Node
-    {
-        return $this->items[0] ?? null;
-    }
-
-    /**
-     * Gets the last node of a list or {@see null} in case of the list is empty.
-     *
-     * @return TNode|null
-     */
-    public function last(): ?Node
-    {
-        $key = \array_key_last($this->items);
-
-        if ($key === null) {
-            return null;
-        }
-
-        return $this->items[$key];
     }
 }
