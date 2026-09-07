f650303f0ca911bac7116ebb9909db737626c38e
SerafimArts <nesk@xakep.ru>
2026-09-07 21:59:16 +0300

    Add comments to the types list AST node

--- libs/types/src/TypesListNode.php
+++ libs/types/src/TypesListNode.php
@@ -5,6 +5,16 @@ declare(strict_types=1);
 namespace TypeLang\Type;
 
 /**
+ * A list of the wrapped type, written in the legacy `[]` notation.
+ *
+ * ```
+ *  Some\Any[]
+ *  ^^^^^^^^   the type the list is made of
+ *
+ *  Some\Any[][]
+ *  ^^^^^^^^^^   a list the outer list is made of
+ * ```
+ *
  * @template T of TypeNode = TypeNode
  *
  * @template-extends WrappingTypeNode<T>
