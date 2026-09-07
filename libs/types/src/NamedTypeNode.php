6a4b7e3e55b4bdce28b1c9ea4ff624c5f5fd5ab5
SerafimArts <nesk@xakep.ru>
2026-09-07 22:00:13 +0300

    Add example comment to the named type AST node

--- libs/types/src/NamedTypeNode.php
+++ libs/types/src/NamedTypeNode.php
@@ -7,6 +7,24 @@ namespace TypeLang\Type;
 use TypeLang\Type\Shape\FieldsListNode;
 use TypeLang\Type\Template\TemplateArgumentListNode;
 
+/**
+ * A type referenced by name, along with whatever the name is followed by.
+ *
+ * ```
+ *  Some\Any<int, string>
+ *  ^^^^^^^^              the name
+ *          ^^^^^^^^^^^^^ the template arguments
+ *
+ *  array{name: string}
+ *  ^^^^^               the name
+ *       ^^^^^^^^^^^^^^ the shape fields
+ *
+ * object{name: string, ...<string, mixed>}
+ * ^^^^^^                                   the name
+ *       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ the shape fields
+ *                         ^^^^^^^^^^^^^^^  the template arguments
+ * ```
+ */
 final class NamedTypeNode extends TypeNode
 {
     /**
@@ -14,7 +32,29 @@ final class NamedTypeNode extends TypeNode
      */
     public function __construct(
         public Name $name,
+        /**
+         * Template arguments of a type, or {@see null} in case of the name is
+         * followed by none.
+         *
+         * The list is never empty: An `Some<>` is no type.
+         *
+         * ```
+         *  Some       // null
+         *  Some<int>  // a list of 1 argument
+         * ```
+         */
         public ?TemplateArgumentListNode $arguments = null,
+        /**
+         * Shape fields of a type, or {@see null} in case of the name is
+         * followed by none. An empty list is not the same as a {@see null}:
+         * A shape may well be written with no fields at all.
+         *
+         * ```
+         *  array           // null: no shape
+         *  array{}         // an empty list: a shape of no fields
+         *  array{a: int}   // a list of 1 field
+         * ```
+         */
         public ?FieldsListNode $fields = null,
         int $offset = 0,
     ) {
