8b3b428081bd69bc1bde852809b47efc18cb5855
SerafimArts <nesk@xakep.ru>
2026-09-07 21:50:27 +0300

    Split `variable` and `this` nodes

---
+++ libs/types/src/ThisNode.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TypeLang\Type;
+
+/**
+ * The type of the object a method is called on, written as a `$this`.
+ *
+ * ```
+ *  callable(): $this
+ * ```
+ *
+ * It is written the way a variable is, but it is a type: It names the object
+ * itself rather than a place a value is kept in. Any other variable is
+ * a {@see VariableNode} and no type at all.
+ */
+final class ThisNode extends TypeNode {}
