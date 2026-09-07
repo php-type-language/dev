cd1ea667aeb5c23a61c2c0c3cea70d86efabc85a
SerafimArts <nesk@xakep.ru>
2026-09-07 22:30:57 +0300

    Drop gt, gte, lt and lte conditions

--- libs/types/src/Condition/LessThanConditionNode.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace TypeLang\Type\Condition;
-
-final class LessThanConditionNode extends Condition {}
--- libs/types/src/Condition/LessThanOrEqualConditionNode.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace TypeLang\Type\Condition;
-
-final class LessThanOrEqualConditionNode extends Condition {}
