08732e5d625ccbca566b3dc3b5338a1fc1a45fbf
SerafimArts <nesk@xakep.ru>
2026-09-07 21:25:29 +0300

    Add wrapping AST type comments with examples

--- libs/types/src/WrappingTypeNode.php
+++ libs/types/src/WrappingTypeNode.php
@@ -5,6 +5,14 @@ declare(strict_types=1);
 namespace TypeLang\Type;
 
 /**
+ * A type (monad) built around another one.
+ *
+ * ```
+ *  ?Some\Any
+ *  ^         the wrapping type
+ *   ^^^^^^^^ the type it wraps
+ * ```
+ *
  * @template T of TypeNode = TypeNode
  */
 abstract class WrappingTypeNode extends TypeNode
