f9df21d5a797cb3b279b30947f296aac83335e9d
SerafimArts <nesk@xakep.ru>
2026-09-07 22:31:25 +0300

    Improve comparison nodes

--- libs/types/src/Condition/NotEqualConditionNode.php
+++ libs/types/src/Condition/NotEqualConditionNode.php
@@ -4,4 +4,11 @@ declare(strict_types=1);
 
 namespace TypeLang\Type\Condition;
 
+/**
+ * Whether the subject is anything but the target.
+ *
+ * ```
+ *  ($value is not int ? string : bool)
+ * ```
+ */
 final class NotEqualConditionNode extends Condition {}
